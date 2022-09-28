d47214a31df04766dad72e2df0f20c26bddc26ab
vgoel <59496579+goelvaibhav105@users.noreply.github.com>
2022-09-28 16:42:09 +0530

    add loading and errro

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -4,9 +4,10 @@ import {useGetTopChartsQuery} from '../redux/services/shazamCore'
 
 const Discover = () => {
   const {data,isFetching, error} = useGetTopChartsQuery();
-  console.log(data,"data")
-    console.log(genres)
+
     const genreTitle = "Pop"
+    if(isFetching) return <Loader title="Loading Songs..."/>
+    if(error) return <Error/>
     return <div className="flex flex-col">
           <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
         <h2 className="font-bold text-3xl text-white text-left">Discover {genreTitle}</h2>
